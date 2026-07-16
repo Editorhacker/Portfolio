c6a6b490eb1c2134907aca949ab5d977d9e3462e
Editorhacker <sutaraditya2807@gmail.com.com>
2026-07-16 23:15:38 +0530

    for Seo

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import VitePluginSitemap from "vite-plugin-sitemap"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePluginSitemap({
+      hostname: "https://aditya-sutar.vercel.app/",
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
+  }
 })
